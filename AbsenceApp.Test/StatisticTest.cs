00a35c8a9c0c24a3c6a99ecf907f3b78eb8191c6
Marwan <marwansaad123@hotmail.com>
2022-05-18 22:40:38 +0200

    Add statistic and Test project with one test method

---
+++ AbsenceApp.Test/StatisticTest.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace AbsenceApp.Test
+{
+    [TestClass]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
--- AbsenceWebApp/Controllers/HomeController.cs
+++ AbsenceWebApp/Controllers/HomeController.cs
@@ -1,14 +1,18 @@
 ﻿using AbsenceAppData;
+using AbsenceWebApp.FileImporter;
 using AbsenceWebApp.FileReader;
 using AbsenceWebApp.Helper;
 using AbsenceWebApp.Models;
 using AbsenceWebApp.Statistics;
 using AbsenceWebApp.ViewModels;
+using ClosedXML.Excel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -27,7 +31,6 @@ namespace AbsenceWebApp.Controllers
             , IFileWebHandler fileWebHandler
             , IAbsenceReportHandler absenceReportHandler
             , IStatisticManager statisticManager
-
             )
         {
             _logger = logger;
@@ -49,16 +52,47 @@ namespace AbsenceWebApp.Controllers
               string FilePathB = _fileWebHandler.SaveFile(model.FileB);
               string StartData = _fileWebHandler.SaveFile(model.StartData);
 
-              var results=  _absenceReportHandler.GetAbsenceReport(FilePathA, FilePathB, StartData).ToList();
+              List<Absence> bsenceReportResult =  _absenceReportHandler.GetAbsenceReport(FilePathA, FilePathB, StartData).ToList();
+                           
+            var SumStatisicBasedOnPrecentage=  _statisticManager.GetMonthStatisticBasedOnPrecentage(3, bsenceReportResult);
 
-              var st1=  _statisticManager.GetMonthStatisticBasedOnPrecentage(3, results);
+             var SumStatisicBasedOnType= _statisticManager.GetAbsenceNumbersWithTypeA(3, bsenceReportResult);
 
-              var st2 = _statisticManager.GetAbsenceNumbersWithTypeA(3, results);
+             var SumStatisicBasedOnContinuousAbsenceDays = _statisticManager.GetContinuousAbsenceForRangeOfDays(4, bsenceReportResult);
 
-              var st3 = _statisticManager.GetcContinuousAbsencForRangeOfDays(4, results);
+                using (XLWorkbook wb = new XLWorkbook())
+                {
+
+                    wb.Worksheets.Add(Common.ToDataTable(bsenceReportResult));
+                    var data = new DataTable();
+                    data.TableName="Statistic";
+                    data.Columns.Add("Summary of statistics");
+
+                    data.Rows.Add("Sum of absence numbers with type A for March");                   
+                    data.Rows.Add(SumStatisicBasedOnType);
+
+                    data.Rows.Add("Sum of continuous absence for range of days for April ");
+                    data.Rows.Add(SumStatisicBasedOnContinuousAbsenceDays);
+
+                    data.Rows.Add("List of absence Ids that have absence mimimum 85 in March ");
+                    foreach (var item in SumStatisicBasedOnPrecentage)
+                    {
+                        data.Rows.Add(item);
+
+                    }
+                    wb.Author = "Marwan";
+                    wb.Worksheets.Add(data);
+
+                    using (MemoryStream stream = new MemoryStream())
+                    {
+                        wb.SaveAs(stream);
+                        return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "AbscenceReport.xlsx");
+                    }
+                }
+
 
             }
-            return View();
+            return View(model);
         }
 
         public IActionResult Privacy()
